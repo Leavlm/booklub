51a83ff1b0d7b456a4c139de6acd24fdd1e73f65
Léa Valmy <lea.valmy@gmail.com>
2023-07-06 16:03:39 +0200

    verifying if author ids are matching

--- add.php
+++ add.php
@@ -2,7 +2,7 @@
 
 require "includes/_database.php";
 require "includes/_functions.php";
-// session_start();
+
 verifyToken();
 
 $query = $dbCo->prepare("INSERT INTO author (author_name) VALUES (:author)");
@@ -10,7 +10,15 @@ $isOk2 = $query->execute([
     "author" => strip_tags($_POST['author'])
 ]);
 
-if($isOk2){
+
+$authorId = $dbCo->lastInsertId();
+
+$query = $dbCo->prepare("SELECT * FROM author WHERE id_author = :authorId");
+$query->execute(["authorId" => $authorId]);
+$author = $query->fetch(PDO::FETCH_ASSOC);
+
+
+if($isOk2 && ($author && $author['id_author'] == $authorId)){
 $query = $dbCo->prepare("INSERT INTO book (title_book, nb_pages, id_author)  VALUES(:title, :pages, :authorId)");                     
 $isOk1 = $query->execute([
     "title" => strip_tags($_POST['title']),
