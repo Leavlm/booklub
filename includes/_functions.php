fa708a45fa9108f88f4673b2c4cb641395ea7d3d
Léa Valmy <lea.valmy@gmail.com>
2023-07-21 10:02:30 +0200

    get dynamic book from db on index page

--- includes/_functions.php
+++ includes/_functions.php
@@ -1,12 +1,36 @@
 <?php 
 session_start();
 
+// -----------------------------
+//   DISPLAY CATALOG
+// -----------------------------
+
+function getCatalog(array $array) :string{
+    foreach($array as $book){
+    return '<li class="card__wrap">
+                    <a class="card__lnk" href="">
+                        // <picture>
+                        //     <source srcset="img/book-blackwater-lg.jpg" media="(min-width: 769px)">
+                        //     <img src="img/book-blackwater.jpg" class="card__img" alt="couverture de livre">
+                        // </picture>
+                        <h3 class="card__ttl">'.$book['title_book'].'</h3>
+                        <p class="card__txt">'.$book['author_name'].'</p>
+                    </a>
+                </li>';}
+}
+
+
+
+
+// -----------------------------
+//   GET VALIDATION OR FAIL MSG 
+// -----------------------------
+
+
 $msgArray = [
     'ok' => 'Vôtre livre a bien été ajouté ! 🥳',
     'ko' => 'Vôtre livre n\'a pas pu être ajouté ! 😱',
     'wrongToken' => 'Le token est erroné ! 😱'
-    
-
 ];
 
 
@@ -19,6 +43,9 @@ function getMsg(array $array): string
     return '';
 };
 
+//---------------
+//   VERIFY TOKEN 
+//---------------
 
 function verifyToken(){
     if(!array_key_exists('token', $_SESSION) || !array_key_exists('token', $_REQUEST) || $_SESSION['token'] !== $_REQUEST['token'] ){
--- index.php
+++ index.php
@@ -1,114 +1,127 @@
+<?php include "includes/_head.php";
+require "includes/_header.php";
+require "includes/_database.php";
+require "includes/_functions.php";
 
+$query = $dbCo->prepare("SELECT `title_book`, `author_name`
+                        FROM `book`
+                        JOIN `author` ON `book`.`id_author` = `author`.`id_author`");
+$query->execute();
+$books = $query->fetchAll();
 
-<?php include "includes/_head.php" ?>
+?>
 
+<main>
+    <section class="intro">
+        <article class="intro__wrap">
+            <h1 class="intro__ttl">Booklub</h1>
+            <p class="intro__txt">Vous en avez marre de chercher <br>votre prochain livre ? <br></p>
+            <a class="cta" href="choose-product.php">
+                <p class="cta__txt">Aidez moi à choisir</p>
+            </a>
+        </article>
+    </section>
 
-<body>
-    <?php require "includes/_header.php";
-    ?>
+    <section>
+        <article class="catalog">
+            <h2 class="catalog__title">Coups de coeur de l'équipe</h2>
+            <ul class="catalog__lst">
 
-    <main>
-        <section class="intro">
-            <article class="intro__wrap">
-                <h1 class="intro__ttl">Booklub</h1>
-                <p class="intro__txt">Vous en avez marre de chercher <br>votre prochain livre ? <br></p>
-                <a class="cta" href="choose-product.php">
-                    <p class="cta__txt">Aidez moi à choisir</p>
-                </a>
-            </article>
-        </section>
-
-        <section>
-            <article class="catalog">
-                <h2 class="catalog__title">Coups de coeur de l'équipe</h2>
-                <ul class="catalog__lst">
-                    <li class="card__wrap">
-                        <a class="card__lnk" href="">
-                            <picture> <source srcset="img/book-blackwater-lg.jpg" media="(min-width: 769px)">
+                <!-- <li class="card__wrap">
+                    <a class="card__lnk" href="">
+                        <picture>
+                            <source srcset="img/book-blackwater-lg.jpg" media="(min-width: 769px)">
                             <img src="img/book-blackwater.jpg" class="card__img" alt="couverture de livre">
                         </picture>
-                            <h3 class="card__ttl">Blackwater IV</h3>
-                            <p class="card__txt">Michael McDowell</p>
-                        </a>
-                    </li>
-                    <li class="card__wrap">
-                        <a class="card__lnk" href="">
-                            <picture><source srcset="img/book-agatha-lg.jpg" media="(min-width: 769px)">
+                        <h3 class="card__ttl">Blackwater IV</h3>
+                        <p class="card__txt">Michael McDowell</p>
+                    </a>
+                </li>
+                <li class="card__wrap">
+                    <a class="card__lnk" href="">
+                        <picture>
+                            <source srcset="img/book-agatha-lg.jpg" media="(min-width: 769px)">
                             <img src="img/book-agatha.jpg" class="card__img" alt="couverture de livre">
                         </picture>
-                            <h3 class="card__ttl">Agatha Raisin 21</h3>
-                            <p class="card__txt">M.C Beaton</p>
-                        </a>
-                    </li>
-                    <li class="card__wrap">
-                        <a class="card__lnk" href="">
-                            <picture><source srcset="img/book-oneofus-lg.jpg" media="(min-width: 769px)">
-                            <img src="img/book-oneofus.jpg" class="card__img" alt="couverture de livre">
-                        </picture>
-                            <h3 class="card__ttl">One of us is dead</h3>
-                            <p class="card__txt">Jeneva Rose</p>
-                        </a>
-                    </li>
-                    <li class="card__wrap">
-                        <a class="card__lnk" href="">
-                            <picture><source srcset="img/book-parfaite-lg.jpg" media="(min-width: 769px)">
-                            <img src="img/book-parfaite.jpg" class="card__img" alt="couverture de livre">
-                        </picture>
-                            <h3 class="card__ttl">Parfaite</h3>
-                            <p class="card__txt">Caroline Kepnes </p>
-                        </a>
-                    </li>
-                    <li class="card__wrap">
-                        <a class="card__lnk" href="">
-                            <picture><source srcset="img/book-oneofus-lg.jpg" media="(min-width: 769px)">
-                            <img src="img/book-oneofus.jpg" class="card__img" alt="couverture de livre">
-                        </picture>
-                            <h3 class="card__ttl">One of us is dead</h3>
-                            <p class="card__txt">Jeneva Rose</p>
-                        </a>
-                    </li>
-                    <li class="card__wrap">
-                        <a class="card__lnk" href="">
-                            <picture><source srcset="img/book-parfaite-lg.jpg" media="(min-width: 769px)">
-                            <img src="img/book-parfaite.jpg" class="card__img" alt="couverture de livre">
-                        </picture>
-                            <h3 class="card__ttl">Parfaite</h3>
-                            <p class="card__txt">Caroline Kepnes </p>
-                        </a>
-                    </li>
-                    <li class="card__wrap">
-                        <a class="card__lnk" href="">
-                            <picture> <source srcset="img/book-blackwater-lg.jpg" media="(min-width: 769px)">
-                                <img src="img/book-blackwater.jpg" class="card__img" alt="couverture de livre">
-                            </picture>
-                            <h3 class="card__ttl">Blackwater IV</h3>
-                            <p class="card__txt">Michael McDowell</p>
-                        </a>
-                    </li>
-                    <li class="card__wrap">
-                        <a class="card__lnk" href="">
-                            <picture><source srcset="img/book-agatha-lg.jpg" media="(min-width: 769px)">
-                                <img src="img/book-agatha.jpg" class="card__img" alt="couverture de livre">
-                            </picture>
-                            <h3 class="card__ttl">Agatha Raisin 21</h3>
-                            <p class="card__txt">M.C Beaton</p>
-                        </a>
-                    </li>
-                </ul>
-
-                <div class="cta__wrap">
-                    <a class="cta cta__position" href="">
-                        <p class="cta__txt--little">Voir plus</p>
+                        <h3 class="card__ttl">Agatha Raisin 21</h3>
+                        <p class="card__txt">M.C Beaton</p>
                     </a>
-                </div>
-                
-            </article>
-    
-            
-        </section>
-    </main>
+                </li>
+                <li class="card__wrap">
+                    <a class="card__lnk" href="">
+                        <picture>
+                            <source srcset="img/book-oneofus-lg.jpg" media="(min-width: 769px)">
+                            <img src="img/book-oneofus.jpg" class="card__img" alt="couverture de livre">
+                        </picture>
+                        <h3 class="card__ttl">One of us is dead</h3>
+                        <p class="card__txt">Jeneva Rose</p>
+                    </a>
+                </li>
+                <li class="card__wrap">
+                    <a class="card__lnk" href="">
+                        <picture>
+                            <source srcset="img/book-parfaite-lg.jpg" media="(min-width: 769px)">
+                            <img src="img/book-parfaite.jpg" class="card__img" alt="couverture de livre">
+                        </picture>
+                        <h3 class="card__ttl">Parfaite</h3>
+                        <p class="card__txt">Caroline Kepnes </p>
+                    </a>
+                </li>
+                <li class="card__wrap">
+                    <a class="card__lnk" href="">
+                        <picture>
+                            <source srcset="img/book-oneofus-lg.jpg" media="(min-width: 769px)">
+                            <img src="img/book-oneofus.jpg" class="card__img" alt="couverture de livre">
+                        </picture>
+                        <h3 class="card__ttl">One of us is dead</h3>
+                        <p class="card__txt">Jeneva Rose</p>
+                    </a>
+                </li>
+                <li class="card__wrap">
+                    <a class="card__lnk" href="">
+                        <picture>
+                            <source srcset="img/book-parfaite-lg.jpg" media="(min-width: 769px)">
+                            <img src="img/book-parfaite.jpg" class="card__img" alt="couverture de livre">
+                        </picture>
+                        <h3 class="card__ttl">Parfaite</h3>
+                        <p class="card__txt">Caroline Kepnes </p>
+                    </a>
+                </li>
+                <li class="card__wrap">
+                    <a class="card__lnk" href="">
+                        <picture>
+                            <source srcset="img/book-blackwater-lg.jpg" media="(min-width: 769px)">
+                            <img src="img/book-blackwater.jpg" class="card__img" alt="couverture de livre">
+                        </picture>
+                        <h3 class="card__ttl">Blackwater IV</h3>
+                        <p class="card__txt">Michael McDowell</p>
+                    </a>
+                </li>
+                <li class="card__wrap">
+                    <a class="card__lnk" href="">
+                        <picture>
+                            <source srcset="img/book-agatha-lg.jpg" media="(min-width: 769px)">
+                            <img src="img/book-agatha.jpg" class="card__img" alt="couverture de livre">
+                        </picture>
+                        <h3 class="card__ttl">Agatha Raisin 21</h3>
+                        <p class="card__txt">M.C Beaton</p>
+                    </a>
+                </li> -->
+            </ul>
 
-    <?php require"footer.php" ?>
+            <div class="cta__wrap">
+                <a class="cta cta__position" href="">
+                    <p class="cta__txt--little">Voir plus</p>
+                </a>
+            </div>
+
+        </article>
+
+
+    </section>
+</main>
+
+<?php require "footer.php" ?>
 <script src="js/search.js"></script>
 </body>
 
