3a54bf01cf20573f2cec8db0fd9f6262e23db9e7
Léa Valmy <lea.valmy@gmail.com>
2023-08-03 17:04:24 +0200

    improving navbar

--- includes/_head.php
+++ includes/_head.php
@@ -14,6 +14,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/474662ed2f.js" crossorigin="anonymous"></script>
     <title>Recherche
     </title>
 </head>
